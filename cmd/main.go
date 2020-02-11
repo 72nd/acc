c0bf25f9ff43434e6813d6caf66c83dbb148e46f
72th <foo@bar.com>
2020-02-11 17:50:22 +0100

    add new function

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 	"gitlab.com/72th/acc/pkg/bimpf"
+	"gitlab.com/72th/acc/pkg/schema"
 	"os"
+	"path"
 )
 
 func main() {
@@ -17,7 +19,7 @@ func main() {
 		},
 		Commands: []*cli.Command{
 			{
-				Name: "bimpf",
+				Name:  "bimpf",
 				Usage: "Bimpf related functions",
 				Action: func(c *cli.Context) error {
 					_ = cli.ShowCommandHelp(c, c.Command.Name)
@@ -37,7 +39,7 @@ func main() {
 								Usage:   "path to bimpf dump json",
 							},
 							&cli.StringFlag{
-								Name: "nc-path",
+								Name:  "nc-path",
 								Usage: "path to nextcloud project folder which is used by Bimpf",
 							},
 						},
@@ -71,7 +73,7 @@ func main() {
 			},
 			{
 				Name:    "documents",
-				Aliases: []string{"doc"},
+				Aliases: []string{"doc", "document"},
 				Usage:   "aggregate all documents associated with a type of business case",
 				Action: func(c *cli.Context) error {
 					return nil
@@ -89,9 +91,76 @@ func main() {
 					},
 				},
 			},
+			{
+				Name:    "new",
+				Aliases: []string{"n", "create"},
+				Usage:   "generates a new acc project with all needed files, use sub-commands to create only a subset",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:        "overwrite",
+						Aliases:     []string{"o"},
+						Usage:       "enable overwrite of existing files",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					schema.NewProject(getFolderPath(c))
+					return nil
+				},
+			},
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
 		logrus.Fatal(err)
 	}
+}
+
+// getPathOrExit checks if the given context contain a string in the args, if not output an error and exit.
+func getPathOrExit(c *cli.Context) string {
+	if c.Args().Len() < 1 || c.Args().First() == "" {
+		logrus.Fatal("path as a argument is needed")
+	}
+	return c.Args().First()
+}
+
+// getFolderPath reads the first argument, validates and formats the folder path.
+// It will also be checked, if the file can be overwritten.
+func getFolderPath(c *cli.Context) string {
+	var pth string
+	if c.Args().Len() == 0 || c.Args().First() == "" {
+		logrus.Info("will use working dir, as no folder path is given as an argument")
+		var err error
+		pth, err = os.Getwd()
+		if err != nil {
+			logrus.Error(err)
+		}
+	}
+	pth = c.Args().First()
+
+	fi, err := os.Stat(pth)
+	if pth != "" {
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		if !fi.IsDir() {
+			logrus.Fatalf("given path (%s) is not a folder")
+		}
+	}
+	if !c.Bool("overwrite") && filesExist(pth) {
+		logrus.Fatal("use -o to overwrite files")
+	}
+	return pth
+}
+
+// filesExist checks if there are no default project files existent.
+// If this is the case, the application will be terminated.
+func filesExist(folderPath string) bool {
+
+	for i := range schema.DefaultProjectFiles {
+		pth := path.Join(folderPath, schema.DefaultProjectFiles[i])
+		if _, err := os.Stat(pth); err == nil {
+			logrus.Infof("file %s exists", pth)
+			return true
+		}
+	}
+	return false
 }
