e139ea5b42b5f6ddc8d1c6b2e060b2a953121532
72th <foo@bar.com>
2020-06-11 01:33:43 +0200

    project and misc added

--- pkg/schema/misc.go
+++ pkg/schema/misc.go
@@ -3,14 +3,16 @@ package schema
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
+	"github.com/creasty/defaults"
 	"github.com/sirupsen/logrus"
 	"gitlab.com/72th/acc/pkg/util"
 	"gopkg.in/yaml.v2"
 )
 
 const DefaultMiscRecordsFile = "misc.yaml"
-const DefaultMiscRecordPrfix = "m-"
+const DefaultMiscRecordPrefix = "m-"
 
 // MiscRecord is a collection of Miscellaneous Record elements.
 type MiscRecords []MiscRecord
@@ -80,6 +82,22 @@ func (m MiscRecords) SearchItems() util.SearchItems {
 	return rsl
 }
 
+// Validate all MiscRecords.
+func (m MiscRecords) Validate() util.ValidateResults {
+	var rsl util.ValidateResults
+	for i := range m {
+		rsl = append(rsl, util.Check(m[i]))
+	}
+	return rsl
+}
+
+// Repopulate all MiscRecords based on the transactions.
+func (m MiscRecords) Repopulate(a Acc) {
+	for i := range m {
+		m[i].Repopulate(a)
+	}
+}
+
 // MiscRecord represents business records which are not invoices or expenses
 // but still important for accounting. Example: A credit note from an insurance.
 type MiscRecord struct {
@@ -91,12 +109,37 @@ type MiscRecord struct {
 	TransactionId string `yaml:"settlementTransactionId" default:"" query:"transaction"`
 }
 
+// NewMiscRecord returns a new MiscRecord element with the default values.
+func NewMiscRecord() MiscRecord {
+	mrc := MiscRecord{}
+	if err := defaults.Set(&mrc); err != nil {
+		logrus.Fatal("error setting defaults for misc record: ", err)
+	}
+	return mrc
+}
+
+// InteractiveNewMiscRecord returns a new MiscRecord based on the user input.
+func InteractiveNewMiscRecord(a Acc, asset string) MiscRecord {
+	logrus.Fatal("interactive new misc record isn't implemented yet")
+	return MiscRecord{}
+}
+
 // SetId generates a unique id for the element if there isn't already one defined.
 func (m *MiscRecord) SetId() {
 	if m.Id != "" {
 		return
 	}
 	m.Id = GetUuid()
+}
+
+// Repopulate MiscRecord based on the transactions.
+func (m *MiscRecord) Repopulate(a Acc) {
+	trn, err := a.Statement().TransactionForDocument(m.Id)
+	if err != nil {
+		logrus.Warnf("there is no transaction for expense \"%s\" associated", m.String())
+		return
+	}
+	m.TransactionId = trn.Id
 }
 
 // GetId returns the id of the MiscRecord.
@@ -109,6 +152,15 @@ func (m MiscRecord) GetIdentifier() string {
 	return m.Identifier
 }
 
+// SearchItem returns a searchable representation of the MiscRecord.
+func (m MiscRecord) SearchItem() util.SearchItem {
+	return util.SearchItem{
+		Name:        fmt.Sprintf("%s (%s)", m.Name, m.Identifier),
+		Type:        m.Type(),
+		Value:       m.Id,
+		SearchValue: fmt.Sprintf("%s %s", m.Identifier, m.Name)}
+}
+
 // String returns a human readable representation of the element.
 func (m MiscRecord) String() string {
 	return fmt.Sprintf("misc record %s (%s)", m.Name, m.Identifier)
@@ -119,11 +171,36 @@ func (m MiscRecord) Type() string {
 	return "MiscRecord"
 }
 
-// SearchItem returns a searchable representation of the MiscRecord.
-func (m MiscRecord) SearchItem() util.SearchItem {
-	return util.SearchItem{
-		Name:        fmt.Sprintf("%s (%s)", m.Name, m.Identifier),
-		Type:        m.Type(),
-		Value:       m.Id,
-		SearchValue: fmt.Sprintf("%s %s", m.Identifier, m.Name)}
+// FileString returns the file name for exporting the misc record as a document.
+func (m MiscRecord) FileString() string {
+	rsl := m.Identifier
+	rsl = strings.ReplaceAll(rsl, " ", "-")
+	rsl = strings.ReplaceAll(rsl, ".", "-")
+	return rsl
+}
+
+// Conditions returns the validation conditions.
+func (m MiscRecord) Conditions() util.Conditions {
+	return util.Conditions{
+		{
+			Condition: m.Id == "",
+			Message:   "unique identifier not set (Id is empty)",
+		},
+		{
+			Condition: m.Identifier == "",
+			Message:   "human readable identifier not set (Identifier is empty)",
+		},
+		{
+			Condition: m.Name == "",
+			Message:   "name not set (Name is empty)",
+		},
+		{
+			Condition: !util.FileExist(m.Path),
+			Message:   fmt.Sprintf("business record document at \"%s\" not found", m.Path),
+		},
+		{
+			Condition: m.Date != "" && util.ValidDate(util.DateFormat, m.Date),
+			Message:   fmt.Sprintf("string \"%s\" could not be parsed with format YYYY-MM-DD", m.Date),
+		},
+	}
 }
--- pkg/schema/project.go
+++ pkg/schema/project.go
@@ -1,6 +1,173 @@
 package schema
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/creasty/defaults"
+	"github.com/sirupsen/logrus"
+	"gitlab.com/72th/acc/pkg/util"
+	"gopkg.in/yaml.v3"
+)
+
+const DefaultProjectsFile = "projects.yaml"
+const DefaultProjectPrefix = "p-"
+
 // Projects represents a collection of multiple Project elements.
 type Projects []Project
 
-type Project struct{}
+// NewProjects returns an empty Projects collection.
+func NewProjects() Projects {
+	return Projects{}
+}
+
+// OpenProjects opens the Projects saved in the YAML file given by the path.
+func OpenProjects(path string) Projects {
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		logrus.Fatalf("error while reading file %s: %s", path, err)
+	}
+	prj := Projects{}
+	if err := yaml.Unmarshal(raw, &prj); err != nil {
+		logrus.Fatalf("error reading (unmarshalling) YAML file %s: %s", path, err)
+	}
+	return prj
+}
+
+// Save writes the element as YAML file to the given path.
+func (p Projects) Save(path string) {
+	SaveToYaml(p, path)
+}
+
+// ProjectById returns the Project with the given id. If no record could be found
+// an error will be returned.
+func (p Projects) ProjectById(id string) (*Project, error) {
+	for i := range p {
+		if p[i].Id == id {
+			return &p[i], nil
+		}
+	}
+	return nil, fmt.Errorf("no project for id \"%s\" found", id)
+}
+
+// ProjectByIdent returns the Project with the given identifier.
+// If no record could be found an error will be returned.
+func (p Projects) ProjectByIdent(ident string) (*Project, error) {
+	for i := range p {
+		if p[i].Identifier == ident {
+			return &p[i], nil
+		}
+	}
+	return nil, fmt.Errorf("no project for ident \"%s\" found", ident)
+}
+
+// GetIdentifiables returns the a sliche of all identifiers. This is used for the
+// identifier suggestion while interactively adding a new Project.
+func (p Projects) GetIdentifiables() []Identifiable {
+	rsl := make([]Identifiable, len(p))
+	for i := range p {
+		rsl[i] = p[i]
+	}
+	return rsl
+}
+
+// SearchItems returns a searchable structure of the Projects. So the user
+// can search for Projects in the interactive mode.
+func (p Projects) SearchItems() util.SearchItems {
+	rsl := make(util.SearchItems, len(p))
+	for i := range p {
+		rsl[i] = p[i].SearchItem()
+	}
+	return rsl
+}
+
+// Validate all Projects.
+func (p Projects) Validate() util.ValidateResults {
+	var rsl util.ValidateResults
+	for i := range p {
+		rsl = append(rsl, util.Check(p[i]))
+	}
+	return rsl
+}
+
+// Project represents a project for a customer.
+type Project struct {
+	Id         string `yaml:"id" default:"1"`
+	Identifier string `yaml:"identifier" default:"p-1"`
+	Name       string `yaml:"name" default:"Building a space rocket"`
+	CustomerId string `yaml:"customerId" default:""`
+}
+
+// NewProject returns a new Project element with the default values.
+func NewProject() Project {
+	prj := Project{}
+	if err := defaults.Set(&prj); err != nil {
+		logrus.Fatal("error setting defaults for project: ", err)
+	}
+	return prj
+}
+
+// InteractiveNewProject returns a new Project based on the user input.
+func InteractiveNewProject(a Acc, asset string) Project {
+	logrus.Fatal("interactive new project isn't implemented yet")
+	return Project{}
+}
+
+// SetId generates a unique id for the element if there isn't already one defined.
+func (p *Project) SetId() {
+	if p.Id != "" {
+		return
+	}
+	p.Id = GetUuid()
+}
+
+// GetId returns the id of the Project.
+func (p Project) GetId() string {
+	return p.Id
+}
+
+// GetIdentifier return the identifier of the Project.
+func (p Project) GetIdentifier() string {
+	return p.Identifier
+}
+
+// SearchItem returns a searchable representation of the Project.
+func (p Project) SearchItem() util.SearchItem {
+	return util.SearchItem{
+		Name:        fmt.Sprintf("%s (%s)", p.Name, p.Identifier),
+		Type:        p.Type(),
+		Value:       p.Id,
+		SearchValue: fmt.Sprintf("%s %s", p.Identifier, p.Name)}
+}
+
+// String returns a human readable representation of the element.
+func (p Project) String() string {
+	return fmt.Sprintf("project %s (%s)", p.Name, p.Identifier)
+}
+
+// Type returns a string with the type name of the element.
+func (p Project) Type() string {
+	return "Project"
+}
+
+// Conditions returns the validation conditions.
+func (p Project) Conditions() util.Conditions {
+	return util.Conditions{
+		{
+			Condition: p.Id == "",
+			Message:   "unique identifier not set (Id is empty)",
+		},
+		{
+			Condition: p.Identifier == "",
+			Message:   "human readable identifier not set (Identifier is empty)",
+		},
+		{
+			Condition: p.Name == "",
+			Message:   "name not set (Name is empty)",
+		},
+		{
+			Condition: p.CustomerId == "",
+			Message:   "customer id not set (CustomerId is empty)",
+		},
+	}
+}
