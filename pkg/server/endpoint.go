1e79b3fbbad5231f12b2eb0cfbadd809fedc4eff
72nd <msg@frg72.com>
2020-11-14 18:40:54 +0100

    Moved API Code to own module

--- pkg/server/endpoint.go
+++ pkg/server/endpoint.go
@@ -2,7 +2,14 @@
 package server
 
 import (
+	"fmt"
+
 	"github.com/72nd/acc/pkg/schema"
+	"github.com/72nd/acc/pkg/server/api"
+	"github.com/labstack/echo/v4"
+	middleware "github.com/labstack/echo/v4/middleware"
+	"github.com/neko-neko/echo-logrus/v2/log"
+	"github.com/sirupsen/logrus"
 )
 
 // Defines the endpoint for the REST interface.
@@ -22,24 +29,179 @@ func NewEndpoint(s *schema.Schema) Endpoint {
 
 // Serve Runs the REST endpoint on the given port.
 func (e *Endpoint) Serve(port int) {
-	//router := e.buildRouter()
-	//http.ListenAndServe(fmt.Sprintf(":%d", port), router)
+	/*
+		swagger, err := GetSwagger()
+		if err != nil {
+			logrus.Fatalf("error loading OpenAPI spec: %s", err)
+		}
+		swagger.Servers = nil
+	*/
+	echo := echo.New()
+	echo.Logger = log.Logger()
+	echo.Use(middleware.Logger())
+	// echo.Use(middleware.OapiRequestValidator(swagger))
+
+	api.RegisterHandlers(echo, e)
+
+	if port == 0 {
+		port = 8000
+	}
+	echo.Logger.Fatal(echo.Start(fmt.Sprintf("0.0.0.0:%d", port)))
 }
 
-/*
-// GenDocs generates the API documentation and writes the result into the given folder.
-// As this function generates two different kind of documentation (JSON and Markdown)
-// two files are written. The two file extensions are added to the given path.
-func (e Endpoint) GenDocs(path string, do-overwrite bool) {
-	router := e.buildRouter()
-	docgen.MarkdownRoutesDoc(router, docgen.MarkdownOpts{
-	})
+// Get all customers
+// (GET /customers)
+func (e *Endpoint) GetCustomers(ctx echo.Context, params api.GetCustomersParams) error {
+	logrus.Info("hoi")
+	return nil
 }
 
-// buildRouter builds the chi router.
-func (e *Endpoint) buildRouter() chi.Router {
-	r := chi.NewRouter();
-	r.Use(render.SetContentType(render.ContentTypeJSON))
-	return r
+// Remove a customer
+// (DELETE /customers/{id})
+func (e *Endpoint) DeleteCustomersId(ctx echo.Context, id string) error {
+	return nil
 }
-*/
+
+// Get a customer by ID
+// (GET /customers/{id})
+func (e *Endpoint) GetCustomersId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Add a customer
+// (POST /customers/{id})
+func (e *Endpoint) PostCustomersId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Update a customer
+// (PUT /customers/{id})
+func (e *Endpoint) PutCustomersId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get all employees
+// (GET /employees)
+func (e *Endpoint) GetEmployees(ctx echo.Context, params api.GetEmployeesParams) error {
+	return nil
+}
+
+// Get all Expenses
+// (GET /expenses)
+func (e *Endpoint) GetExpenses(ctx echo.Context, params api.GetExpensesParams) error {
+	return nil
+}
+
+// Remove a Expense
+// (DELETE /expenses/{id})
+func (e *Endpoint) DeleteExpensesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get a Expense by ID
+// (GET /expenses/{id})
+func (e *Endpoint) GetExpensesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Add a Expense
+// (POST /expenses/{id})
+func (e *Endpoint) PostExpensesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Update a Expense
+// (PUT /expenses/{id})
+func (e *Endpoint) PutExpensesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get all invoices
+// (GET /invoices)
+func (e *Endpoint) GetInvoices(ctx echo.Context, params api.GetInvoicesParams) error {
+	return nil
+}
+
+// Remove a Invoice
+// (DELETE /invoices/{id})
+func (e *Endpoint) DeleteInvoicesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get a Invoice by ID
+// (GET /invoices/{id})
+func (e *Endpoint) GetInvoicesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Add a Invoice
+// (POST /invoices/{id})
+func (e *Endpoint) PostInvoicesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Update a Invoice
+// (PUT /invoices/{id})
+func (e *Endpoint) PutInvoicesId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get all Miscellaneous Records
+// (GET /misc_records)
+func (e *Endpoint) GetMiscRecords(ctx echo.Context, params api.GetMiscRecordsParams) error {
+	return nil
+}
+
+// Remove a Miscellaneous Record
+// (DELETE /misc_records/{id})
+func (e *Endpoint) DeleteMiscRecordsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get a Miscellaneous Record by ID
+// (GET /misc_records/{id})
+func (e *Endpoint) GetMiscRecordsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Add a Miscellaneous Record
+// (POST /misc_records/{id})
+func (e *Endpoint) PostMiscRecordsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Update a Miscellaneous Record
+// (PUT /misc_records/{id})
+func (e *Endpoint) PutMiscRecordsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get all Projects
+// (GET /projects)
+func (e *Endpoint) GetProjects(ctx echo.Context, params api.GetProjectsParams) error {
+	return nil
+}
+
+// Remove a Project
+// (DELETE /projects/{id})
+func (e *Endpoint) DeleteProjectsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Get a Project by ID
+// (GET /projects/{id})
+func (e *Endpoint) GetProjectsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Add a Project
+// (POST /projects/{id})
+func (e *Endpoint) PostProjectsId(ctx echo.Context, id string) error {
+	return nil
+}
+
+// Update a Project
+// (PUT /projects/{id})
+func (e *Endpoint) PutProjectsId(ctx echo.Context, id string) error {
+	return nil
+}
