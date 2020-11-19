d72ecee4d533eb53ecea858ef4cff8c009e1fbe8
72nd <msg@frg72.com>
2020-11-19 16:34:28 +0100

    Ident as a structure

--- src/schema/common.rs
+++ src/schema/common.rs
@@ -1,3 +1,6 @@
+use super::date::Date;
+use super::record::RecordType;
+
 use std::fmt;
 
 use serde::{Deserialize, Serialize};
@@ -25,5 +28,80 @@ impl ID {
 impl fmt::Display for ID {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+/// Short but unique identifier for a record. The purpose of this identifier is to provide the
+/// user a more memorable id than the internally used UUID of the ID object. For some users this
+/// will easy the integration of Acc within any existing work flows as this field has no
+/// requirements to it's content besides it has to be unique.
+///
+/// The structure provides some extra functionality to ease the handling with the ident system of
+/// the Solutionsbüro.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Ident(String);
+
+impl Ident {
+    /// Returns a new Identifier based on a freely-chosen string.
+    pub fn from<S: Into<String>>(ident: S) -> Self {
+        Self(ident.into())
+    }
+
+    /// Returns a new Identifier containing the appropriate prefix followed by the given number.
+    pub fn from_n(rtype: RecordType, number: u64) -> Self {
+        Self(format!("{}-{}", Ident::prefix_by_type(rtype), number))
+    }
+
+    /// Returns a new Identifier containing the appropriate prefix followed by the given year (two
+    /// digit representation) and number.
+    pub fn from_year_n(rtype: RecordType, date: Date, number: u64) -> Self {
+        Self(format!(
+            "{}-{}-{}",
+            Ident::prefix_by_type(rtype),
+            date.year_2d(),
+            number
+        ))
+    }
+
+    /// Returns the Solutionsbüro default prefix for any given record type. Each type has it's
+    /// individual prefix to clearly distinguish between the different types of records.
+    fn prefix_by_type<'a>(rtype: RecordType) -> &'a str {
+        match rtype {
+            RecordType::Customer => "c",
+            RecordType::Employee => "y",
+            RecordType::Expense => "e",
+            RecordType::ExpenseCategory => "ec",
+            RecordType::Invoice => "i",
+            RecordType::Misc => "m",
+            RecordType::Project => "p",
+            RecordType::Transaction => "t",
+        }
+    }
+}
+
+impl fmt::Display for Ident {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Ident;
+    use crate::schema::Date;
+    use crate::schema::RecordType;
+
+    #[test]
+    fn test_from_n() {
+        assert_eq!("e-23", Ident::from_n(RecordType::Expense, 23).to_string());
+    }
+
+    #[test]
+    fn test_from_year_n() {
+        let date = Date::from_ymd(2019, 10, 2);
+        assert_eq!(
+            "e-19-23",
+            Ident::from_year_n(RecordType::Expense, date, 23).to_string()
+        );
     }
 }
