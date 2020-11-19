49f8c26819eef11e3b876dbc76695533bdb9fd59
72nd <msg@frg72.com>
2020-11-19 16:33:58 +0100

    updated public stuff

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -10,6 +10,8 @@ mod record;
 mod relation;
 mod transaction;
 
-pub use common::*;
-pub use entity::*;
-pub use expense::*;
+pub use common::{Ident, ID};
+pub use date::Date;
+pub use entity::{Customer, Employee, Entity};
+pub use expense::Expense;
+pub use record::{Record, RecordType};
